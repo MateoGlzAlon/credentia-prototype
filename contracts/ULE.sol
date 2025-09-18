e8e12571f3dac047b6c3463d118ae35352d074fb
i565647 <mateo.gonzalez.alonso@gmail.com>
2025-09-18 11:48:57 +0200

    modified contracts and updated mockData

--- contracts/ULE.sol
+++ contracts/ULE.sol
@@ -5,24 +5,26 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Credentia is ERC721URIStorage {
+    error NotOwner();
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string public institution = "Universidad de Leon";
-    string public titleName = "Ingenieria Informatica";
-
     mapping(address => string) public allowedWallets;
 
-    constructor() ERC721("Academic Title", "TIT") {
+    constructor() ERC721("Universidad de Leon", "ULE") {
         allowedWallets[
             0xe4f1638f1E34dF36D0B3523b4402A89F1478f0B1
-        ] = "Universidad de Leon";
+        ] = "Rector";
+        allowedWallets[
+            0x3fD0842f6ccd38d57E5aDAbda61EeefBfB07e583
+        ] = "Secretaria";
     }
 
     function awardItem(
         address recipient,
         string memory metadataURI
-    ) public returns (uint256) {
+    ) public onlyAllowedWallets returns (uint256) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
@@ -32,5 +34,16 @@ contract Credentia is ERC721URIStorage {
         _setTokenURI(newItemId, metadataURI);
 
         return newItemId;
+    }
+
+    modifier onlyAllowedWallets() {
+        string memory role = allowedWallets[msg.sender];
+        if (
+            keccak256(abi.encodePacked(role)) != keccak256(abi.encodePacked("Rector")) &&
+            keccak256(abi.encodePacked(role)) != keccak256(abi.encodePacked("Secretaria"))
+        ) {
+            revert NotOwner();
+        }
+        _;
     }
 }
